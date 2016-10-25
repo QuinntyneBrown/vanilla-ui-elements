b4e860970075fb52c92fc441c7c7e955dce59223
Quinntyne Brown <quinntynebrown@gmail.com>
2016-10-24 20:26:33 -0400

    update

---
+++ pane/pane.ts
@@ -0,0 +1,15 @@
+﻿https://developer.mozilla.org/en-US/docs/Web/Web_Components/Custom_Elements
+
+class PaneElement extends HTMLElement {
+
+    constructor() {
+        super();
+        
+    }
+
+    attachedCallback() {
+
+    }
+}
+
+(document as any).registerElement('v-pane', PaneElement);
